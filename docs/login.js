a3bc8f2bd059807f4a7371bc77c72b2885c1d852
Mattx8y <wrscr.surpertop@gmail.com>
2018-08-01 12:01:13 -0400

    Update login.js

--- docs/login.js
+++ docs/login.js
@@ -1,3 +1,3 @@
 function login() {
-  window.open("http://www.mccitynetwork.tk/log-in/discord","Log-In","width=600,height=700")
+  window.open("http://www.mccitynetwork.tk/log-in/discord","Log-In","width=500,height=600")
 }
