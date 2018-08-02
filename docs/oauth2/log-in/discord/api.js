02f0af80b6ca0e2ec6abb5e12752824c11046f19
Mattx8y <wrscr.surpertop@gmail.com>
2018-08-01 21:18:48 -0400

    Update api.js

--- docs/oauth2/log-in/discord/api.js
+++ docs/oauth2/log-in/discord/api.js
@@ -1,6 +1,10 @@
 var access_denied = window.location.search.includes("error=access_denied")
+var has_code = window.location.search.includes("code=")
 
 if (access_denied) {
   alert("Access Denied")
 }
-window.close();
+
+if (!has_code) {
+  window.close();
+}
